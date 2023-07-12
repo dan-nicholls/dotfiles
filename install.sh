e3277570e4903735a2e7d709fbd34f8a39f06d4e
Dan Nicholls <dan.nicholls@trurecognition.io>
2023-07-12 10:17:05 +1000

    Updated install and stow ignore

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Zsh Setup
+sudo apt update
+sudo apt install zsh
+sudo chsh -s /usr/bin/zsh
+
+# tmux Setup
+sudo apt install tmux
+git clone https://github.com/tmux-plugins/tpm ./.tmux/plugins/tpm
+
+
